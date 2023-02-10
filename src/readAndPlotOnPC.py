3dfb30fc74ec075f1ba978f6731fb5ee52ec08a1
sydneygoth <sgothenquist@hotmail.com>
2023-02-10 07:00:41 -0800

    comments fixed

--- src/readAndPlotOnPC.py
+++ src/readAndPlotOnPC.py
@@ -14,7 +14,11 @@ from matplotlib import pyplot
 """!
 The plot function takes in a data list that is 2 items wide (one for t, one for x). These are then stored line by line into two seperate lists, t and x, and each value is converted into a float.
 The pyplot function is then used to plot the two lists against each other.
+
+ * @brief Plots data received from the serial port.
+ * @param[in] input the data to be plotted.
 """
+
 def plot_data(input):
     t = []
     x = []
@@ -36,15 +40,10 @@ def plot_data(input):
     pyplot.show()
 
 with (serial.Serial("COM5",115200) as ser):
-    #ser.flush() #clear Input buffer
-    #ser.in_waiting()#NUM of CHArs in Buffer
-
-    #ser.read(n)
-    #ser.readlines()#read until \n or \r
-    #ser.readlines()
-    
 
     """!
+
+ Main function that reads and plots data from the serial port.
     first while loop is set up for doing multiple plots in a row. Since that is not needed for this project it wont be used, but the structure is here for future applications.
     The second while loop is true so long as the data is being sent in. Once the end of data flag has been sent, we exit the second while loop. Before entering this loop, we initialize the data list, and clear out the data stream.
     Inside the second while loop, we take the next line from the stream of data, seperate it at the comma and store it in the next row of a list called data.
